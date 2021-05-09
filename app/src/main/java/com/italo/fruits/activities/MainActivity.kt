fbd0e7570ddfdf0104311446b43d52c436c9511c
italo <italord.melo@gmail.com>
2021-05-09 00:39:54 -0300

    packages

---
+++ app/src/main/java/com/italo/fruits/activities/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.italo.fruits.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.italo.fruits.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
